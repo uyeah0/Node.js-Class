fbcc4fed39b3026cc724936a9bb38a18811d0637
uyeah0 <oki93639@gmail.com>
2022-03-08 14:25:22 +0900

    test 모듈 분리

--- NodeExample/ch02_test1.js
+++ NodeExample/ch02_test1.js
@@ -6,3 +6,10 @@ for(let i = 1; i <= 1000; i++){
 }
 console.timeEnd('duration_sum');
 console.log('1부터 1000까지 더한 결과물 : %d', result);
+
+console.log('현재 실행한 파일의 이름 : %s', __filename);
+console.log('현재 실행한 파일의 패스 : %s', __dirname);
+
+// dir() 메소드 사용하기
+let Person = {name:"양정원", age:19};
+console.dir(Person);
